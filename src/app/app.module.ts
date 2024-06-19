26d1f29c60823f17aed3c7d719724ab72c93e843
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-06-19 22:05:41 +0800

    feat: 畫面切版

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { NavItemComponent } from './components/nav-item/nav-item.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, NavItemComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
