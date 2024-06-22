5595d9376806c7542e93ed106cf6edae17f0ec9c
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-06-22 15:55:16 +0800

    chore: 新增google map 套件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { NavItemComponent } from './components/nav-item/nav-item.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule, NavItemComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    NavItemComponent,
+    GoogleMapsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
