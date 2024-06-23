b4255afeb68bce373147114b20969829b9fd9a23
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-06-23 22:56:02 +0800

    feat: 加入地圖畫面&標記

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
 @Component({
@@ -8,8 +8,37 @@ import { initFlowbite } from 'flowbite';
 })
 export class AppComponent implements OnInit {
   title = 'e-parking';
+  // 初始預設在台北車站
+  initialCenter = { lat: 25.0474428, lng: 121.5170955 };
+  options: google.maps.MapOptions = {
+    center: { ...this.initialCenter },
+    zoom: 14,
+    mapTypeControl: false,
+    fullscreenControl: false,
+    streetViewControl: false,
+  };
+  infoWindow = new google.maps.InfoWindow();
+  markerPositions!: google.maps.LatLngLiteral;
+  @ViewChild('map') map: ElementRef | undefined;
 
   ngOnInit(): void {
     initFlowbite();
+    this.getUserPosition();
+  }
+
+  getUserPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        this.options = {
+          ...this.options,
+          center: pos,
+        };
+        this.markerPositions = pos;
+      });
+    }
   }
 }
