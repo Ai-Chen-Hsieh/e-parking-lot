41adcdef21f497c00e8442b7b54f58908d0899bb
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-06-30 17:26:45 +0800

    加入標記

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { Service } from './service/service';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +12,42 @@ export class AppComponent implements OnInit {
   title = 'e-parking';
   // 初始預設在台北車站
   initialCenter = { lat: 25.0474428, lng: 121.5170955 };
+  // map設定
   options: google.maps.MapOptions = {
     center: { ...this.initialCenter },
     zoom: 14,
     mapTypeControl: false,
     fullscreenControl: false,
     streetViewControl: false,
+    zoomControl: false,
   };
-  infoWindow = new google.maps.InfoWindow();
-  markerPositions!: google.maps.LatLngLiteral;
-  @ViewChild('map') map: ElementRef | undefined;
+  // 標記設定
+  selfMarkerPositions!: google.maps.LatLngLiteral;
+  // 訊息視窗
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
+  currentInfoWindow = {
+    id: 0,
+    name: '',
+    area: '',
+    address: '',
+    payex: '',
+    serviceTime: '',
+    totalcar: 0,
+    totalmotor: 0,
+    ChargingStation: 0,
+    location: {
+      lat: 0,
+      lng: 0,
+    },
+  };
+
+  allStation: any[] = [];
+  service = inject(Service);
 
   ngOnInit(): void {
     initFlowbite();
     this.getUserPosition();
+    this.getStation();
   }
 
   getUserPosition() {
@@ -37,8 +61,45 @@ export class AppComponent implements OnInit {
           ...this.options,
           center: pos,
         };
-        this.markerPositions = pos;
+        this.selfMarkerPositions = pos;
       });
     }
+  }
+
+  getStation() {
+    this.service.getAllStation().subscribe((res) => {
+      let allStationPosition = res.data.park
+        .filter(
+          (i: {
+            EntranceCoord: { EntrancecoordInfo: { Xcod: any; Ycod: any }[] };
+          }) => i.EntranceCoord.EntrancecoordInfo !== undefined,
+        )
+        .map((item: any) => {
+          return {
+            id: item.id,
+            name: item.name,
+            area: item.area,
+            address: item.address,
+            payex: item.payex,
+            serviceTime: item.serviceTime,
+            totalcar: item.totalcar,
+            totalmotor: item.totalmotor,
+            ChargingStation: item.ChargingStation,
+            location: {
+              lat: Number(item.EntranceCoord.EntrancecoordInfo[0]?.Xcod),
+              lng: Number(item.EntranceCoord.EntrancecoordInfo[0]?.Ycod),
+            },
+          };
+        });
+      this.allStation = [...allStationPosition];
+    });
+  }
+
+  openInfoWindow(marker: MapMarker, item: any) {
+    this.currentInfoWindow = {
+      ...item,
+    };
+    console.log('current', this.currentInfoWindow);
+    this.infoWindow!.open(marker);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { NavItemComponent } from './components/nav-item/nav-item.component';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     RouterModule,
+    FormsModule,
     NavItemComponent,
     GoogleMapsModule,
   ],
