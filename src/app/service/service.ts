ec4ae320a925cc8ce2e4d4b548108ef7a00fef4a
Ai-Chen-Hsieh <a0903109727@gmail.com>
2024-07-08 17:07:18 +0800

    fix: 調整service

--- src/app/service/service.ts
+++ src/app/service/service.ts
@@ -1,6 +1,5 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MAP_KEY } from 'src/env/env';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,11 +8,6 @@ import { Observable } from 'rxjs';
 export class Service {
   baseUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=`;
   http = inject(HttpClient);
-  getChargeStation(address: string): Observable<any> {
-    const url = `${this.baseUrl}${address}&key=${MAP_KEY}`;
-    return this.http.get(url);
-  }
-
   getAllStation(): Observable<any> {
     return this.http.get('../../assets/taipei-parking-info.json');
   }
